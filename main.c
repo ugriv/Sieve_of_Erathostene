b3eb1b6ae901fcdb3f5a21589f4393cac7e90c45
ugriv <ugo.rivard@hotmail.com>
2016-10-20 23:15:13 -0400

    Added runtime, fixed some data types
    Included lines to count runtime and changed data type of some variables

--- main.c
+++ main.c
@@ -9,6 +9,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
+#include <time.h>
 
 #define LIMIT 100000000 //Check numbers up to LIMIT (100M)
 #define PRIME 10000000 //Store prime numbers (10M)
@@ -21,18 +22,21 @@
 
 int main(){
 
-	int i, j, count;
+	unsigned long long int i, j, count;
 	//static int number[LIMIT], prime[PRIME];
 	int *number = malloc(LIMIT * sizeof(int));
 	int *prime = malloc(PRIME * sizeof(int));
 	//Large arrays need to be declared statically or dynamically using malloc
 	//(both methods work up to ~100M integers arrays)
+	clock_t starttime, endtime;
+	double runtime;
 
 	FILE *output, *output2;
 
 	output = fopen(OUT1,"w");
 	if(DEBUG>=1) output2 = fopen(OUT2,"w");
 
+	starttime = clock();
 	//Fill the number array starting from 2 to LIMIT+1
 	for(i=0;i<LIMIT;i++){
 		number[i]=i+2;
@@ -44,7 +48,7 @@ int main(){
 			if(DEBUG>=1) fprintf(output2,"%d should be prime\n",number[i]);
 			while(j<LIMIT){
 				j+=number[i]; //Multiples are NOT prime..
-				if(j<=LIMIT && number[j-2]!=-1 && DEBUG >=1) fprintf(output2,"%d\t%d\n",j,number[j-2]);
+				if(j<=LIMIT && number[j-2]!=-1 && DEBUG >=1) fprintf(output2,"%I64u\t%d\n",j,number[j-2]);
 				if(j<=LIMIT) number[j-2]=-1; //.. so we flag them
 				//Conditionnal IF added because number[j-2] can fall out of bounds..
 				//..and generate errors because we're dealing with random values in memory
@@ -61,7 +65,7 @@ int main(){
 		}
 	}
 
-	fprintf(output,"Number of prime numbers up to %d is : %d\n\nList:\n\n",LIMIT,count);
+	fprintf(output,"Number of prime numbers up to %d is : %I64u\n\nList:\n\n",LIMIT,count);
 
 	for(i=0;i<count;i++){
 		fprintf(output,"%d\n",prime[i]); //Print the primes
@@ -69,5 +73,9 @@ int main(){
 
 	fclose(output);
 	if(DEBUG>=1) fclose(output2);
+	endtime = clock();
+	runtime = ((double) (endtime - starttime)) / CLOCKS_PER_SEC;
+	printf("Runtime is %lf\n", runtime);
+	getchar();
 	return 0;
 }
